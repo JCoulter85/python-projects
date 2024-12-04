8da10df751457a4744b2c81999eaf5b5e857c624
James Coulter <jimcoulter85@gmail.com>
2024-12-03 21:33:01 -0800

    Step 3: Prepare dataset and fix encoding issue

--- data_analysis_project.py
+++ data_analysis_project.py
@@ -1,7 +1,27 @@
 import pandas as pd
 
 # Load the dataset
-data = pd.read_csv("Active_Businesses_-_County_Data")
+data = pd.read_csv("F:/Coding/Active_Businesses_-_County_Data.csv", encoding="utf-8")
 
 # display the first few rows
 print (data.head())
+
+# display column names
+print("Column Names:")
+print(data.columns)
+
+# check data structure and types
+print("\nData information:")
+print(data.info())
+
+#check for missing values
+print("\nMissing Values:")
+print(data.isnull().sum())
+
+# basic statistics for numeric columns
+print("\nBasic Statistics:")
+print(data.describe())
+
+# unique values in each column
+for col in data.columns:
+        print(f"{col}: {data[col].nunique()} unique values")
