dffbc1082f952bac662a1ca0a1666c184ae3b027
James Coulter <jimcoulter85@gmail.com>
2024-12-03 20:34:51 -0800

    starting python again: hello.py

---
+++ Hello.py
@@ -0,0 +1,3 @@
+#Getting back on the horse
+name = input("What is your name? ") #This will prompt the user to enter their name
+print(f"Hello, {name}! Welcome to your Python journey!") #greats the user
